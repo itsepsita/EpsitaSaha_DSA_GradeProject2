80d099b9d36d415bad28c3c291688f4f6382224e
itsepsita <77191477+itsepsita@users.noreply.github.com>
2023-12-26 12:13:08 +0530

    Add files via upload
    The Group Assignment 1

---
+++ Department/src/DeparmentalRoles/HrDepartment.java
@@ -0,0 +1,20 @@
+package DeparmentalRoles;
+public class HrDepartment extends SuperDepartment{
+
+		String departmentName()
+		{
+			return "HR Department";
+		}
+		String getTodaysWork()
+		{
+			return "Fill today's timesheet and mark your attendance";
+		}
+		String getWorkDeadline()
+		{
+			return "Complete by EOD";
+		}
+		String doActivity()
+		{
+			return "team Lunch";
+		}
+}
